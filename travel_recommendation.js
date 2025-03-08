2dbc9a5283b88710ae9aeb87670221cade0060d1
aab92 <araujob.antonio@gmail.com>
2025-03-08 11:15:37 +0100

    search results WIP

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,5 @@
+const resultElement = document.getElementById('result');
+
 function showResult() {
   user_input = document.getElementById('searchInput').value.toLowerCase();
   if (user_input.includes("beach")) {
@@ -10,12 +12,26 @@ function showResult() {
     user_input = "countries";
   }
   
-  // fetch('travel_recommendation_api.json')
-  fetch('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json')
+  fetch('travel_recommendation_api.json')
+  //fetch('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json')
     .then(response => response.json())
     .then(data => {
       if (user_input === "beaches") {
+        data.beaches.forEach( (beach) => {
+          // TODO
+          resultElement.innerHTML = '<div id="result_entry">';
+          resultElement.innerHTML += `<img src="${beach.img}"/>`;
+          resultElement.innerHTML += '</div>;
+          /*for (const condition in conditionsCount) {
+            resultElement.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`;
+          }*/
+          console.log(`found beach : ${beach.name.toLowerCase()}`);
+        });
       } else if (user_input === "temples") {
+        data.temples.forEach( (temple) => {
+          // TODO
+          console.log(`found temple : ${temple.name.toLowerCase()}`);
+        });
       } else if (user_input === "countries") {
         data.countries.forEach( (country) => {
           // TODO
